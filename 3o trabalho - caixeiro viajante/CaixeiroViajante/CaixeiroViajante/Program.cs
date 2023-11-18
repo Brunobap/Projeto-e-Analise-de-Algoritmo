cc4f0a7e088a4aceef5348fe3252f33b27b19c26
Bruno Ribeiro Batista <bruno@engsupport.eti.br>
2023-11-17 23:05:27 -0300

    17/11/2023

--- 3o trabalho - caixeiro viajante/CaixeiroViajante/CaixeiroViajante/Program.cs
+++ 3o trabalho - caixeiro viajante/CaixeiroViajante/CaixeiroViajante/Program.cs
@@ -1,6 +1,11 @@
 ﻿
 using CaixeiroViajante;
+using System.Collections;
 
 Grafo g = new Grafo("D:\\Programacao\\Projeto-e-Analise-de-Algoritmo\\3o trabalho - caixeiro viajante\\CaixeiroViajante\\CaixeiroViajante\\Teste.txt");
 
-Console.WriteLine("ola");
+Object[] solucaoTentErr = Resolucoes.TentativaErro(g);
+Console.Write("Solução por Tentativa e Erro:\n - Peso do caminho encontrado: " + solucaoTentErr[0] + "\n - Ordem do caminho encontrado:");
+foreach (int v in (ArrayList)solucaoTentErr[1]) Console.Write(" "+v);
+
+Console.WriteLine("\n\n\n*****************************              acabou aqui                        *************************************\n\n\n");
--- 3o trabalho - caixeiro viajante/CaixeiroViajante/CaixeiroViajante/Resolucoes.cs
+++ 3o trabalho - caixeiro viajante/CaixeiroViajante/CaixeiroViajante/Resolucoes.cs
@@ -25,14 +25,76 @@ namespace CaixeiroViajante
         public static object[] TentativaErro(Grafo g)
         {
             Object[] saida = new Object[2];
+
+            ArrayList todosOsCaminhos = new ArrayList();
+
+            ArrayList start = new ArrayList();
+            start.Add(0);
+
+            todosOsCaminhos.Add(start);
+
+            int aux = 0;
+
+            // Pegar todos os caminhos possíveis
+            while (aux < todosOsCaminhos.Count)
+            {
+                ArrayList caminho = (ArrayList)todosOsCaminhos[aux];
+                int ultimo = caminho.Count-1;
+                foreach (Aresta a in g.ArestaDe((int)caminho[ultimo]))
+                {
+                    if (!caminho.Contains(a.Destino()))
+                    {
+                        ArrayList clone = (ArrayList) caminho.Clone();
+                        clone.Add(a.Destino());
+                        todosOsCaminhos.Add(clone);
+                    }
+                }
+                if (ultimo < g.NumVertices() - 1)
+                    todosOsCaminhos.Remove(caminho);
+                else
+                {
+                    aux++;
+                    caminho.Add(0);
+                }
+            }
+
+            // Pegar o peso do 1o caminho na lista, e tirar esse caminho da lista
             double pesoCaminho = 0;
-            int[] arrayCaminho = new int[g.NumVertices()];
+            ArrayList arrayCaminho = (ArrayList)todosOsCaminhos[0];
+            for (int i = 0; i < g.NumVertices(); i++)
+            {
+                int origem = (int)arrayCaminho[i], destino = (int)arrayCaminho[i + 1]; 
+                Aresta passo = (Aresta)g.arestasEntre(origem,destino)[0];
+                pesoCaminho += passo.Peso();
+            }
+            todosOsCaminhos.RemoveAt(0);
+
+            // Comparar os caminhos entre si
+            foreach (ArrayList caminho in todosOsCaminhos)
+            {
+                // Pegar o peso do caminho 
+                double pesoComp = 0;
+                for (int i = 0; i < g.NumVertices(); i++)
+                {
+                    int origem = (int)caminho[i], destino = (int)caminho[i + 1];
+                    Aresta passo = (Aresta)g.arestasEntre(origem, destino)[0];
+                    pesoComp += passo.Peso();
+                }
+
+                // Achou um caminho menor, ele agora é o escolhido
+                if (pesoComp < pesoCaminho)
+                {
+                    arrayCaminho = caminho;
+                    pesoCaminho = pesoComp;
+                }
+            }
 
 
             saida[0] = pesoCaminho;
             saida[1] = arrayCaminho;
             return saida;
         }
+
         public static object[] AlgoritmoGenetico(Grafo g)
         {
             Object[] saida = new Object[2];
