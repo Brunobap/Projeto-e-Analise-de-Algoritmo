ae25a5618a58b86bc5a42d85b77f41b385b5c7da
Bruno Ribeiro Batista <bruno@engsupport.eti.br>
2023-12-17 16:04:11 -0300

    12/12/2023

--- 3o trabalho - caixeiro viajante/CaixeiroViajante/CaixeiroViajante/Program.cs
+++ 3o trabalho - caixeiro viajante/CaixeiroViajante/CaixeiroViajante/Program.cs
@@ -3,7 +3,7 @@ using CaixeiroViajante;
 using System.Collections;
 
 
-String path = ".\\..\\..\\..\\Teste12.txt";
+String path = ".\\..\\..\\..\\avaliacao18.txt";
 Grafo g = new Grafo(path);
 Console.WriteLine("Arquivo usado: "+path);
 
@@ -11,8 +11,8 @@ Object[] solucaoTentErr = Resolucoes.TentativaErro(g);
 Console.Write("Solução por Tentativa e Erro:\n - Peso do caminho encontrado: " + solucaoTentErr[0] + "\n - Ordem do caminho encontrado:");
 foreach (int v in (int[])solucaoTentErr[1]) Console.Write(" " + v);
 
-Object[] solucaoHeurist = Resolucoes.AlgoritmoGenetico(g);
+/*Object[] solucaoHeurist = Resolucoes.AlgoritmoGenetico(g);
 Console.Write("\n\nSolução por Algoritmo Genético:\n - Peso do caminho encontrado: " + solucaoHeurist[0] + "\n - Ordem do caminho encontrado:");
-foreach (int v in (int[])solucaoHeurist[1]) Console.Write(" " + v);
+foreach (int v in (int[])solucaoHeurist[1]) Console.Write(" " + v);*/
 
 Console.WriteLine("\n\n\n*****************************              acabou aqui                        *************************************\n\n\n");
--- 3o trabalho - caixeiro viajante/CaixeiroViajante/CaixeiroViajante/obj/Debug/net6.0/CaixeiroViajante.AssemblyInfo.cs
+++ 3o trabalho - caixeiro viajante/CaixeiroViajante/CaixeiroViajante/obj/Debug/net6.0/CaixeiroViajante.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CaixeiroViajante")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+583baee3706bd7032a4d2b8127b996053544642b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CaixeiroViajante")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CaixeiroViajante")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
